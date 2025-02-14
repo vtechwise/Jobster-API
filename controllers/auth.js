7a477e9e34641ac68a096e390885d559d2a38876
victor <oguntayovictor65@gmail.com>
2025-02-14 15:13:38 +0100

    update register and login controller response

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,7 +6,15 @@ const { BadRequestError, UnauthenticatedError } = require("../errors");
 const register = async (req, res) => {
   const user = await User.create({ ...req.body });
   const token = user.createJWT();
-  res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
+  res.status(StatusCodes.CREATED).json({
+    user: {
+      name: user.name,
+      email: user.email,
+      location: user.location,
+      lastNmae: user.lastName,
+      token,
+    },
+  });
 };
 
 const login = async (req, res) => {
@@ -26,7 +34,15 @@ const login = async (req, res) => {
 
   const token = user.createJWT();
 
-  res.status(StatusCodes.OK).json({ user: { name: user.name }, token });
+  res.status(StatusCodes.OK).json({
+    user: {
+      name: user.name,
+      email: user.email,
+      location: user.location,
+      lastNmae: user.lastName,
+      token,
+    },
+  });
 };
 
 module.exports = {
